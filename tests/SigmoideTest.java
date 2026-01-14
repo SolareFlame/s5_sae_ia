a5c92321a5ddd95448a52401756c560dc68be92b
BenjaminScheffer <benjamin.scheffer7@etu.univ-lorraine.fr>
2026-01-14 15:22:03 +0100

    Classe de test des fonctions d'activations

---
+++ tests/SigmoideTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SigmoideTest {
+
+    @Test
+    public void testSigmoide(){
+        var sigmoide = new Sigmoide();
+        assertEquals(0.26894, sigmoide.evaluate(-1.0),1e-5);
+        assertEquals(0.37754, sigmoide.evaluate(-0.5),1e-5);
+        assertEquals(0.5, sigmoide.evaluate(0));
+        assertEquals(0.62246, sigmoide.evaluate(0.5),1e-5);
+        assertEquals(0.73106, sigmoide.evaluate(1.0),1e-5);
+
+        assertEquals(0.19661, sigmoide.evaluateDer(sigmoide.evaluate(-1.0)),1e-5);
+        assertEquals(0.235, sigmoide.evaluateDer(sigmoide.evaluate(-0.5)),1e-5);
+        assertEquals(0.25, sigmoide.evaluateDer(sigmoide.evaluate(0)),1e-5);
+        assertEquals(0.235, sigmoide.evaluateDer(sigmoide.evaluate(0.5)),1e-5);
+        assertEquals(0.19661, sigmoide.evaluateDer(sigmoide.evaluate(1.0)),1e-5);
+    }
+}
---
+++ tests/TangenteHyperboliqueTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TangenteHyperboliqueTest {
+
+    @Test
+    public void testTangente(){
+        var tangente = new TangenteHyperbolique();
+        assertEquals(-0.76159, tangente.evaluate(-1.0),1e-5);
+        assertEquals(-0.46212, tangente.evaluate(-0.5),1e-5);
+        assertEquals(0, tangente.evaluate(0));
+        assertEquals(0.46212, tangente.evaluate(0.5),1e-5);
+        assertEquals(0.76159, tangente.evaluate(1.0),1e-5);
+
+        assertEquals(0.41997, tangente.evaluateDer(tangente.evaluate(-1.0)),1e-5);
+        assertEquals(0.78645, tangente.evaluateDer(tangente.evaluate(-0.5)),1e-5);
+        assertEquals(1, tangente.evaluateDer(tangente.evaluate(0)),1e-5);
+        assertEquals(0.78645, tangente.evaluateDer(tangente.evaluate(0.5)),1e-5);
+        assertEquals(0.41997, tangente.evaluateDer(tangente.evaluate(1.0)),1e-5);
+    }
+}
